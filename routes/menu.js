d3876bb9ad4c99b90942a20bee264b2085b36c4c
Mohamed Hosam Sabry <mohamed20191705017@cis.asu.edu.org>
2021-04-05 08:33:19 +0200

    some of the closings of the projects

--- routes/menu.js
+++ routes/menu.js
@@ -6,12 +6,25 @@ var balance =0;
 
      //test dataset 
 const items=[
-{name:'Sushi type 1  ',price:10 , picture:'static/images/item1.jpeg',checked:false, number:0},
-{name:'Sushi type 2  ',price:1000 , picture:'static/images/item3.jpeg',checked:false, number:1},
-{name:'Sushi type 3  ',price:32 , picture:'static/images/item1.jpeg',checked:false, number:2},
-{name:'Sushi type 4  ',price:156 , picture:'static/images/item3.jpeg',checked:false, number:3},
-{name:'Sushi type 5  ',price:166, picture:'static/images/item1.jpeg',checked:false, number:4},
-{name:'Sushi type 6  ',price:88, picture:'static/images/item3.jpeg',checked:false, number:5}
+{name:'Fish',price:10 , picture:'static/images/item1.jfif',checked:false, number:0, height:'150px', width:'200px'},
+{name:'Malomma',price:53 , picture:'static/images/item2.jfif',checked:false, number:1, height:'150px', width:'200px'},
+{name:'Howai',price:32 , picture:'static/images/item3.jfif',checked:false, number:2, height:'150px', width:'200px'},
+{name:'Chiaww',price:156 , picture:'static/images/item4.jfif',checked:false, number:3, height:'150px', width:'200px'},
+{name:'Michanna',price:166, picture:'static/images/item5.jfif ',checked:false, number:4, height:'150px', width:'200px'},
+{name:'Blommins',price:88, picture:'static/images/item6.jfif',checked:false, number:5, height:'150px', width:'200px'},
+{name:'Fish Large',price:10 , picture:'static/images/item1.jfif',checked:false, number:6, height:'200px', width:'240px'},
+{name:'Malomma Large',price:53 , picture:'static/images/item2.jfif',checked:false, number:7, height:'200px', width:'240px'},
+{name:'Howai Large',price:32 , picture:'static/images/item3.jfif',checked:false, number:8, height:'200px', width:'240px'},
+{name:'Chiaww Large',price:156 , picture:'static/images/item4.jfif',checked:false, number:9, height:'200px', width:'240px'},
+{name:'Michanna Large',price:166, picture:'static/images/item5.jfif ',checked:false, number:10, height:'200px', width:'240px'},
+{name:'Blommins Large',price:88,  picture:'static/images/item6.jfif',checked:false, number:11, height:'200px', width:'240px'},
+{name:'Fish Small',price:10 , picture:'static/images/item1.jfif',checked:false, number:12 , height:'100px', width:'120px'},
+{name:'Malomma Small',price:53 ,picture:'static/images/item2.jfif',checked:false, number:13, height:'100px', width:'120px'},
+{name:'Howai Small',price:32 , picture:'static/images/item3.jfif',checked:false, number:14, height:'100px', width:'120px'},
+{name:'Chiaww Small',price:156 ,  picture:'static/images/item4.jfif',checked:false, number:15, height:'100px', width:'120px'},
+{name:'Michanna Small',price:166,  picture:'static/images/item5.jfif ',checked:false, number:16, height:'100px', width:'120px'},
+{name:'Blommins Small',price:88,  picture:'static/images/item6.jfif',checked:false, number:17, height:'100px', width:'120px'}
+
 ]
 
 
@@ -40,11 +53,25 @@ var req3=req.body.req2;
 var req4=req.body.req3;
 var req5=req.body.req4;
 var req6=req.body.req5;
+var req7=req.body.req6;
+var req8=req.body.req7;
+var req9=req.body.req8;
+var req10=req.body.req9;
+var req11=req.body.req10;
+var req12=req.body.req11;
+var req13=req.body.req12;
+var req14=req.body.req13;
+var req15=req.body.req14;
+var req16=req.body.req15;
+var req17=req.body.req16;
+var req18=req.body.req17;
+
+
 
 var checkIndex=[]
-console.log(req1, req2, req3, req4, req5, req6);
 
-var reqs=[req1,req2,req3,req4,req5,req6];
+
+var reqs=[req1,req2,req3,req4,req5,req6, req7,req8,req9,req10,req11,req12,req13,req14,req15,req16,req17, req18];
 
 for( var i=0; i<reqs.length; i++){
 if(reqs[i]=="on"){
@@ -53,14 +80,7 @@ checkIndex.push(i);
 }
 
 }
-//price to int 
-// var f=parseInt(item1, 10);
-// var s =parseInt(item2, 10);
 
-
-
-// options.push({name:i1, price:f}, 
-//   {name:i2, price:s });
 
 
 //making an array for the checked items 
